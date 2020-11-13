ff0d02895663581d892c84e0006a81bdbad3869c
kdent <kdent@seaglass.com>
2020-11-12 18:47:09 -0800

    (Chapter 5) Perl script to delete messages from the queue based on the sender or recipient's email address.

---
+++ pfdel.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+#
+# Send contents of the real inforeply.pl to a browser.
+# This is to workaround the fact that I cannot configure
+# the web server to provide *.pl files as files rather
+# than treating them as cgi scripts.
+#
+
+use strict;
+
+my $FILE = "/home2/seaglas1/public_html/postfix/pfdel.txt";
+
+print "Content-type: text/plain\r\n\r\n";
+
+if (! open(FH, $FILE) ) {
+	print "Sorry, I am unable to deliver the script.\n";
+	print $!;
+	exit;
+}
+
+while (<FH>) {
+	chomp;
+	print;
+	print "\n";
+}
+
